2ac044553d0d9e95d576edfbd8be4c37d8ad31bc
Melody-Le <tanmui1992@gmail.com>
2022-12-11 10:30:47 +0800

    feat: add postman

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 const bcrypt = require("bcrypt");
+const ipaddr = require("ipaddr.js");
 const UserModel = require("../models/userModel");
 const validator = require("../validations/userValidation");
+const fetch = require("node-fetch");
 const controller = {
   login: async (req, res) => {
     const validationResults = validator.login.validate(req.body);
@@ -48,6 +50,34 @@ const controller = {
         res.status(200).json({ message: "Success Login", user });
       });
     });
+  },
+  getCountryCode: async (req, res) => {
+    // const ip = "192.168.0.188";
+
+    let remoteAddress = req.ip;
+    if (ipaddr.isValid(remoteAddress)) {
+      const addr = ipaddr.parse(remoteAddress);
+      console.log(addr.isIPv4MappedAddress());
+      if (addr.kind() === "ipv6" && addr.isIPv4MappedAddress()) {
+        remoteAddress = addr.toIPv4Address().toString();
+        console.log(remoteAddress);
+      }
+    }
+    // const access_key = "883abb2024e3f13213aded435afe4994";
+    // const url = `http://api.ipstack.com/${ip}?access_key=${access_key}`;
+
+    // const fetch_res = await fetch(
+    //   `http://ipinfo.io/${req.ip}?token=5fe0b07e8725b4`
+    // )
+    //   .then(function (response) {
+    //     return response.json();
+    //   })
+    //   .then(function (payload) {
+    //     // console.log(payload.location.country_code);
+    //     console.log(payload);
+    //   });
+    // const fetch_data = await fetch_res.json();
+    res.send("heehe");
   },
 };
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/authController");
 const router = express.Router();
 
 router.post("/login", authController.login);
+router.post("/country-code", authController.getCountryCode);
 
 module.exports = router;
